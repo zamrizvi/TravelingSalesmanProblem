6ca499a2119c5f4939dda39a74eb24bbf2a7c356
Zamir Rizvi <zamir.u.rizvi@gmail.com>
2023-08-31 02:03:08 -0500

    Created the WGUPS Package File.csv and cleaned up the data.

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+#Zamir Rizvi
+#ID: 010508494
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import csv
 
+def read_csv(file_path):
+    with open(file_path) as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            print(row)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+csv_file_path = 'WGUPS Package File.csv'
+read_csv(csv_file_path)
